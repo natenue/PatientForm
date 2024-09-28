7415c533ea27c8b5e2ca8abfc7e347344b45da28
nateneu <ashie.n@northeastern.edu>
2024-09-28 08:44:52 -0800

    first commit

---
+++ src/patientapplication/PatientApplication.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patientapplication;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author NATE
+ */
+public class PatientApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Registeration registrationForm = new Registeration();
+
+        // Set the form to be visible
+        registrationForm.setVisible(true);
+        
+
+    }
+    
+}
